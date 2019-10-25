a50c64435d54930a454e324994e291cc55e43671
bohu <bohu@google.com>
2019-10-25 18:32:43 +0800

    RMX2001: Disable codec2
    BUG: 133178866
    Change-Id: Ie6698604357adfb7c36d6655ea8a42aecad6b219
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -1,4 +1,5 @@
 # OMX
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     debug.stagefright.omx_default_rank.sw-audio=1 \
-    debug.stagefright.omx_default_rank=0
+    debug.stagefright.omx_default_rank=0 \
+    debug.stagefright.ccodec=0
