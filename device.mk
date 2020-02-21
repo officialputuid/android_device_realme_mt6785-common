964b6f4048e6f046b22239703665d55bd9290c36
SamarV-121 <samarvispute121@gmail.com>
2020-02-21 14:54:48 +0530

    RMX2001: Build libsuspend and android.hardware.health@2.0
     * dependency of kpoc_charger
     * also remove useless libsysenv_system.so
    
    Change-Id: Ibdd34dfd492e2855e3594bad3e68f555ce2e64d5
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- device.mk
+++ device.mk
@@ -77,6 +77,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/keylayout/touchpanel.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/touchpanel.kl
 
+# KPOC
+PRODUCT_PACKAGES += \
+    libsuspend \
+    android.hardware.health@2.0
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.RMX2001
