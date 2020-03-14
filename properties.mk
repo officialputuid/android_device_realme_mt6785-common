9ec5011b0a9b0ebaca82e4ee9d3f19a80c559bab
Lucas Dupin <dupin@google.com>
2020-03-14 15:01:44 -0700

    mt6785-common: Disable blur by default
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>
    Change-Id: If28a2b702597a7bfc331ae77dcb2e3f4ec07d3fc

--- properties.mk
+++ properties.mk
@@ -1,7 +1,8 @@
 # Blurs
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.surface_flinger.supports_background_blur=1 \
-    ro.sf.blurs_are_expensive=1
+    ro.sf.blurs_are_expensive=1 \
+    persist.sys.sf.disable_blurs=1
 
 # Dex2oat
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
