ea14d2587a944a22a657eb9cfdd30983d5806261
Weilun Du <wdu@google.com>
2020-08-21 14:45:14 -0700

    RMX2001: rro_overlays: Add TetheringOverlay
    BUG: 163800715
    Signed-off-by: Weilun Du <wdu@google.com>
    Change-Id: Ia0834f5215ff8c3b13effa591bd2f84d93419a06
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- device.mk
+++ device.mk
@@ -105,3 +105,7 @@ PRODUCT_BOOT_JARS += \
 
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml
+
+# Wi-Fi
+PRODUCT_PACKAGES += \
+    TetheringConfigOverlay
