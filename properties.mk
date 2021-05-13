55e77d63c6385a59c95d55cf8956052f92e384b1
unknown <cs.officialputuid@gmail.com>
2021-05-13 10:49:15 +0800

    mt6785-common: overlay: Change padding status bar for Nusantara [NAD]
    Signed-off-by: officialputuid <officialputuid@hack.id>

--- properties.mk
+++ properties.mk
@@ -18,7 +18,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 
 # Display
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.sf.lcd_density=420
+    ro.sf.lcd_density=426
 
 # IMS
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
