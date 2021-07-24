8c7ae56a1ffa16f8d7549ba52050e7c8a25461ad
Lunarixus <Nathan@lunarixus.dev>
2021-07-24 17:00:35 +0800

    mt6785-common: configs: Added pixel 2016 xml
    * This allows us to use unlimited backup in Google Photos infinitely.
    
    Change-Id: I461b6057baddb090ca94e838bca9d13a76d15fc8

--- mt6785.mk
+++ mt6785.mk
@@ -65,6 +65,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.fingerprint.xml
 
+# Google Photos
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/pixel_2016_exclusive.xml
+
 # HIDL
 PRODUCT_PACKAGES += \
     libhidltransport \
