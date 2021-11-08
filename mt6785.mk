fb79549322e47a8a841232069e615b80983c88a5
officialputuid <officialputuid@hack.id>
2021-11-08 13:34:57 +0800

    mt6785-common: Build realme Dirac
    Change-Id: I834ed6f5890d2a3a4001b949adee705204703f40

--- mt6785.mk
+++ mt6785.mk
@@ -25,6 +25,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Call proprietary blob setup
 $(call inherit-product, vendor/realme/mt6785-common/mt6785-common-vendor.mk)
 
+# realme Dirac
+$(call inherit-product-if-exists, packages/apps/RealmeDirac/dirac.mk)
+
 # Shipping API
 PRODUCT_SHIPPING_API_LEVEL := 29
 
