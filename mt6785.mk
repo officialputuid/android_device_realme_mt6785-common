2763cc3dc219c922a31092110a56d45d1bab173e
officialputuid <officialputuid@hack.id>
2021-11-08 13:27:52 +0800

    mt6785-common: Fix minor mistakes
    Change-Id: I127566033b9985193cd4cee56c0e8d6166c4c12b

--- mt6785.mk
+++ mt6785.mk
@@ -144,7 +144,7 @@ PRODUCT_BOOT_JARS += \
     mediatek-ims-common \
     mediatek-telecom-common \
     mediatek-telephony-base \
-    mediatek-telephony-common \
+    mediatek-telephony-common
 
 PRODUCT_PACKAGES += \
     ImsServiceBase
