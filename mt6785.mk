99efdb3531e35c01907a5f2e5207c4733836daf9
Bowgo Tsai <bowgotsai@google.com>
2021-11-16 13:09:47 +0800

    mt6785-common: Removing GSI keys
    Those keys will be embedded into VtsSecurityAvb on host side
    instead, to verify the GSI image used on the device.
    
    Bug: 149806769
    Test: build and checks those keys are removed from
          /recovery/root/first_stage_ramdisk/avb/
    Change-Id: Id0166d1c4dd07067bff49c28ca3469b5ca74d7cf

--- mt6785.mk
+++ mt6785.mk
@@ -16,9 +16,6 @@
 
 COMMON_PATH := device/realme/mt6785-common
 
-# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
